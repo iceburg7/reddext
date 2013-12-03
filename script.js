0fc6e78b32d09b9289f22cf48ae40adf7b7b38e9
iceburg7 <ecburgess1@gmail.com>
2013-12-02 21:17:54 -0500

    trying to nest divs

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 	
 	$(".content").append("<p>test</p>");
 	
-	$(".content").append("<div class='extWrapper'></div>");
+	$(".content").append("<div class='extWrapper'>");
+	$(".content").append("<div class='extLeft'></div>");
+	$(".content").append("<div class='extCenter'></div>");
+	$(".content").append("<div class='extRight'></div>");
+	$(".content").append("</div>");
 	
 	$(".extWrapper").click(function(){
 	
